e5648010fc888af0452beffecd443cf462509118
Hanis Dhuzuki <67252046+hanisdhuzuki@users.noreply.github.com>
2025-09-10 17:46:28 +0800

    Update riotlitestreamlit.py
    avoid redundan cv2

--- streamlit/riotlitestreamlit.py
+++ streamlit/riotlitestreamlit.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import cv2
+#import cv2
 import mediapipe as mp
 from handtrack import opposition_exercise, stretch_exercise
 
@@ -74,5 +74,6 @@ while run:
 
 # ---------- Cleanup ----------
 cap.release()
+
 
 
